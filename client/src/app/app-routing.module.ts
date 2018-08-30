fecf0d0d0bb2f2fe8b3df0f87886bd81dea6e223
Marcos <kinhom3@hotmail.com>
2018-08-29 22:34:42 -0300

    update login and register

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+import { NotAuthGuard } from './guards/notAuth.guard';
+import { AuthGuard } from './guards/auth.guard';
+
+//Routes
+const appRoutes: Routes = [
+    {
+      path: '',
+      component: HomeComponent // Default Route
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent, // Dashboard Route,
+      canActivate: [AuthGuard] // User must be logged in to view this route
+    },
+    {
+      path: 'register',
+      component: RegisterComponent, // Register Route
+      canActivate: [AuthGuard] // User must be logged in to view this route
+    },
+    {
+      path: 'login',
+      component: LoginComponent, // Login Route
+      canActivate: [NotAuthGuard] // User must NOT be logged in to view this route
+    },
+
+    {
+      path: 'ticket',
+      component: TicketComponent, // Ticket Route,
+      canActivate: [AuthGuard] // User must be logged in to view this route
+    },
+    { path: '**', component: HomeComponent } // "Catch-All" Route
+  ];
+
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(appRoutes),
     CommonModule
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { LoginComponent } from './components/login/login.component';
@@ -9,6 +13,13 @@ import { TicketComponent } from './components/ticket/ticket.component';
 import { CommentComponent } from './components/comment/comment.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CondominioComponent } from './components/condominio/condominio.component';
+import { HomeComponent } from './components/home/home.component';
+import {MatButtonModule, MatCheckboxModule, MatCardModule, MatToolbarModule, MatInputModule,
+MatFormFieldModule } from '@angular/material';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { NotAuthGuard } from './guards/notAuth.guard';
 
 @NgModule({
   declarations: [
@@ -18,15 +29,26 @@ import { CondominioComponent } from './components/condominio/condominio.componen
     TicketComponent,
     CommentComponent,
     DashboardComponent,
-    CondominioComponent
+    CondominioComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpModule,
+    ReactiveFormsModule,
+    FormsModule,
+    BrowserAnimationsModule,
     MatButtonModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule
+
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, NotAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
